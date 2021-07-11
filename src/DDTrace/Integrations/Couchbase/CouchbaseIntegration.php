0d05ae551a2726d77ebee33704570b1fad15cd3d
Charlie Chang <charlie@idxbroker.com>
2021-07-10 23:31:56 -0700

    WIP: Update couchbase integration

--- src/DDTrace/Integrations/Couchbase/CouchbaseIntegration.php
+++ src/DDTrace/Integrations/Couchbase/CouchbaseIntegration.php
@@ -3,6 +3,7 @@
 namespace DDTrace\Integrations\Couchbase;
 
 use DDTrace\Integrations\Integration;
+use DDTrace\Obfuscation;
 use DDTrace\SpanData;
 use DDTrace\Type;
 
@@ -23,34 +24,32 @@ class CouchbaseIntegration extends Integration
      */
     public function init()
     {
-        \DDTrace\trace_method('Couchbase\Bucket', 'remove', function (SpanData $span, $args) use ($integration) {
-            $this->addSpanDefaultMetadata(span, 'Bucket', 'remove');
-            $span->meta['couchbase.remove'] = '';
-        });
 
-        \DDTrace\trace_method('Couchbase\Bucket', 'insert', function (SpanData $span, $args) use ($integration) {
-            $this->addSpanDefaultMetadata(span, 'Bucket', 'insert');
-            $span->meta['couchbase.insert'] = '';
-        });
+        $this->traceCommand('remove');
+        $this->traceCommand('insert');
+        $this->traceCommand('upsert');
+        $this->traceCommand('replace');
+        $this->traceCommand('get');
 
-        \DDTrace\trace_method('Couchbase\Bucket', 'upsert', function (SpanData $span, $args) use ($integration) {
-            $this->addSpanDefaultMetadata(span, 'Bucket', 'upsert');
-            $span->meta['couchbase.upsert'] = '';
+        \DDTrace\trace_method('Couchbase\Bucket', 'query', function (SpanData $span, $args) {
+            if (isset($args[0])) {
+                $queryType = $this->getQueryType($args[0]);
+                $this->addSpanDefaultMetadata($span, 'Bucket', 'query');
+                $span->resource = $this->getQueryValue($args[0], $queryType);
+            }
         });
+    }
 
-        \DDTrace\trace_method('Couchbase\Bucket', 'replace', function (SpanData $span, $args) use ($integration) {
-            $this->addSpanDefaultMetadata(span, 'Bucket', 'replace');
-            $span->meta['couchbase.replace'] = '';
-        });
+    public function traceCommand($command)
+    {
+        $integration = $this;
+        \DDTrace\trace_method('Bucket', $command, function (SpanData $span, $args) use ($integration, $command) {
+            $integration->addSpanDefaultMetadata($span, 'Bucket', $command);
+            if (!is_array($args[0])) {
+                $span->meta['Bucket.query'] = $command . ' ' . Obfuscation::toObfuscatedString($args[0]);
+            }
 
-        \DDTrace\trace_method('Couchbase\Bucket', 'get', function (SpanData $span, $args) use ($integration) {
-            $this->addSpanDefaultMetadata(span, 'Bucket', 'get');
-            $span->meta['couchbase.get'] = '';
-        });
-
-        \DDTrace\trace_method('Couchbase\Bucket', 'query', function (SpanData $span, $args) use ($integration) {
-            $this->addSpanDefaultMetadata(span, 'Bucket', 'query');
-            $span->meta['couchbase.query'] = '';
+            $integration->markForTraceAnalytics($span, $command);
         });
     }
 
@@ -69,5 +68,70 @@ class CouchbaseIntegration extends Integration
         $span->type = Type::COUCHBASE;
     }
 
+    /**
+     * @param SpanData $span
+     * @param string $command
+     */
+    public function markForTraceAnalytics(SpanData $span, $command)
+    {
+        $commandsForAnalytics = [
+            'insert',
+            'upsert',
+            'replace',
+            'get',
+        ];
 
+        if (in_array($command, $commandsForAnalytics)) {
+            $this->addTraceAnalyticsIfEnabled($span);
+        }
+    }
+
+    /**
+     * Get query value.
+     *
+     * @param ViewQuery|N1qlQuery|SpatialViewQuery|SearchQuery  $query
+     * @param string $type
+     * @return string The query value.
+     */
+    public function getQueryValue($query, $type)
+    {
+        switch ($type) {
+            case 'n1ql':
+                return json_encode($query->options);
+                break;
+            case 'view':
+            case 'spatialview':
+                return json_encode($query->encode());
+                break;
+            case 'search':
+                return json_encode($query);
+                break;
+            default:
+                return get_class($query);
+                break;
+        }
+    }
+
+    /**
+     * Get query type
+     *
+     * @param ViewQuery|N1qlQuery|SpatialViewQuery|SearchQuery $query
+     *
+     * @return string
+     */
+    public function getQueryType($query)
+    {
+        if ($query instanceof \Couchbase\N1qlQuery) {
+            return 'n1ql';
+        }
+        if ($query instanceof \Couchbase\ViewQuery) {
+            return 'view';
+        }
+        if ($query instanceof \Couchbase\searchQuery) {
+            return 'search';
+        }
+        if ($query instanceof \Couchbase\SpatialViewQuery) {
+            return 'spatialview';
+        }
+    }
 }
